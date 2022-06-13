0c5802d4a0b9504fe48443cf9fdec140990860df
Mindaugas <mindaugas.g@rrr.lt>
2022-06-13 22:26:22 +0300

    Update 2022-06-07

---
+++ _ide_helper_models.php
@@ -0,0 +1,218 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Auction
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $book_id
+ * @property int $enabled
+ * @property int $quantity
+ * @property int $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Book $book
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereBookId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereEnabled($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereQuantity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Auction whereUserId($value)
+ */
+	class Auction extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Author
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string|null $description
+ * @property string|null $birthday
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read string $full_name
+ * @method static \Illuminate\Database\Eloquent\Builder|Author newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Author onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereBirthday($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Author whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Author withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Author withoutTrashed()
+ */
+	class Author extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $name
+ * @property string|null $description
+ * @property string|null $iban
+ * @property int|null $year
+ * @property string|null $pages
+ * @property string|null $format
+ * @property string|null $language
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $sku
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property int $viewed_count
+ * @property string|null $image
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auction[] $auctions
+ * @property-read int|null $auctions_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Author[] $authors
+ * @property-read int|null $authors_count
+ * @property-read \App\Models\Category $category
+ * @method static \Database\Factories\BookFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Book onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereFormat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereIban($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book wherePages($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereSku($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereViewedCount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereYear($value)
+ * @method static \Illuminate\Database\Query\Builder|Book withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Book withoutTrashed()
+ */
+	class Book extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Category
+ *
+ * @property int $id
+ * @property int|null $category_id
+ * @property string $name
+ * @property int $active
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|Category[] $categories
+ * @property-read int|null $categories_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|Category[] $childrenCategories
+ * @property-read int|null $children_categories_count
+ * @property-read Category|null $parentCategory
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Category onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Category withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Category withoutTrashed()
+ */
+	class Category extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Role
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
+ */
+	class Role extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $blocked
+ * @property \Illuminate\Support\Collection|null $settings
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auction[] $auctions
+ * @property-read int|null $auctions_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Role[] $roles
+ * @property-read int|null $roles_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User blocked()
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User unblocked()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereBlocked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereSettings($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
